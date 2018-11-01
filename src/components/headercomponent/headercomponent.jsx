ab8cf5ffbc4f04c7610f9180e2a3ff5a43f64d99
RGHuynh <randall.g.huynh@gmail.com>
2018-11-01 14:54:39 -0400

    chore: setup header

---
+++ src/components/headercomponent/headercomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './headercomponent.css';
+
+export default function HeaderComponent() {
+    return(
+        <div className="header--wrapper">
+            
+        </div>
+    );
+}
+
