7a2f4abd9cfcfef18bdc5826b7c41cc8b861f816
RGHuynh <randall.g.huynh@gmail.com>
2018-11-02 13:40:09 -0400

    chore: create previewSection component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import HeaderComponent from './components/headercomponent/headercomponent'
+import HeaderComponent from './components/headercomponent/headercomponent';
+import PreviewSectionComponent from './components/previewSectionComponent/previewSectionComponent';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
         <div className="App">
             <HeaderComponent />
+            <PreviewSectionComponent />
         </div>
     );
   }
---
+++ src/components/previewSectionComponent/previewSectionComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './previewSectionComponent.css';
+
+export default function PreviewSectionComponent(){
+  return(
+    <section className="preview--wrapper">
+      <h1 className="preview-heading__size preview-heading__color">See The Best Restaurant In Your Area</h1>
+    </section>
+  );
+}
