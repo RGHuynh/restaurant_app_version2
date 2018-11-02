a637993b8e3e523468f20cdb1975f0e979ac2018
RGHuynh <randall.g.huynh@gmail.com>
2018-11-02 13:02:32 -0400

    style: headercomponent

--- src/components/headercomponent/headercomponent.jsx
+++ src/components/headercomponent/headercomponent.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import logo from '../../assets/pictures/logo.svg';
 
 import './headercomponent.css';
 
 export default function HeaderComponent() {
     return(
         <div className="header--wrapper">
-            <form id="restaurant-search-form">
+            <img className="logo__size logo__position" src={logo} ></img>
+            <form id="restaurant-search-form" className="restaurant-search-form__position">
                 <input type="text" className="form-input"></input>
-                <button form="btn btn-primary restaurant-search-form">SEARCH</button>
+                <br></br>
+                <button form="restaurant-search-form" className="btn btn-primary btn__color btn__position">SEARCH</button>
             </form>
         </div>
     );
