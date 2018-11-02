e7acc3c34344bac9f76af27f0666fe1c9bcb1fdd
RGHuynh <randall.g.huynh@gmail.com>
2018-11-02 10:20:57 -0400

    implementing commit: header component

--- src/components/headercomponent/headercomponent.jsx
+++ src/components/headercomponent/headercomponent.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import './headercomponent.css';
 
 export default function HeaderComponent() {
     return(
         <div className="header--wrapper">
-            
+            <form id="restaurant-search-form">
+                <input type="text" className="form-input"></input>
+                <button form="btn btn-primary restaurant-search-form">SEARCH</button>
+            </form>
         </div>
     );
 }
